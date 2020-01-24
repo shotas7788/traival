e3898c827b181c135e1af995ac06ff1350b3f8b2
shotas7788 <shotas7788@vivaldi.net>
2020-01-24 06:12:25 +0000

    create_post_destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   
   before_action :authenticate_user!
+  before_action :set_post, only: %i(show destroy)
   
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
   end
   
   def show
-    @post = Post.find_by(id: params[:id])
   end
   
   def new
@@ -27,10 +27,24 @@ class PostsController < ApplicationController
       flash[:alert] = "投稿に失敗しました"
     end
   end
+  
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
 
   private
+  
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
+    end
+    
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   
     # ==========postsコントローラ、アクションののルーティング設定==========
-    resources :posts, only: %i(new create index show) do
+    resources :posts, only: %i(new create index show destroy) do
       resources :photos, only: %i(create)
     end
     # ==========postsコントローラ、アクションののルーティング設定==========
