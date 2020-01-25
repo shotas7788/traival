dae0af217a9c93a4eb9d87c623b97a67420de18e
shotas7788 <shotas7788@vivaldi.net>
2020-01-25 06:11:41 +0000

    add_username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_044709) do
+ActiveRecord::Schema.define(version: 2020_01_25_055332) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_044709) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "profile_photo"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
